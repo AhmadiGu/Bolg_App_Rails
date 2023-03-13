ee51ac7cf7787ee152d438b84625de2f27516ac6
AhmadiGu <ahmadigulalai76@gmail.com>
2023-03-13 16:41:24 +0500

    Added controller, view and route

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,10 @@
+class PostsController < ApplicationController
+  def index
+    @user = User.find_by(id: params[:user_id])
+    @posts = Post.where(author_id: params[:user_id])
+  end
+
+  def show
+    @post = Post.find_by(id: params[:id])
+  end
+end
