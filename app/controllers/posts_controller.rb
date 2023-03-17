c8d0a6deee2cf57998a735eb9d1ceb12a48eb3bd
AhmadiGu <ahmadigulalai76@gmail.com>
2023-03-17 17:56:44 +0500

    Added create comment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,23 @@ class PostsController < ApplicationController
   def show
     @post = Post.find_by(id: params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit(:title, :text).merge(author_id: current_user.id, comments_counter: 0,
+                                                                       likes_counter: 0))
+
+    respond_to do |format|
+      format.html do
+        if @post.save
+          redirect_to user_post_path(current_user, @post)
+        else
+          redirect_to new_user_post_path(current_user)
+        end
+      end
+    end
+  end
 end
